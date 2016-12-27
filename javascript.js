9299e689895f2a0a57ac87fe792bbadf762980e8
b02902032 <b02902032@ntu.edu.tw>
2016-12-27 16:09:15 +0800

    v1 add comment

--- javascript.js
+++ javascript.js
@@ -8,6 +8,69 @@ $(function(){
         wheelUp = 0,
         wheelDown = 0;
 
+    //Map the sections:
+    $scrollItems.each(function(index,ele) { $(ele).attr("debog",index).data("pos",index); });
+
+    // Bind to scroll
+    $(window).bind('scroll',upPos);
+    
+    // Mouse wheel down
+    $(document).on('mousewheel DOMMouseScroll', function (e) {
+        //prevent the default mousewheel scrolling
+        e.preventDefault();
+        //get the delta to determine the mousewheel scrol UP and DOWN
+        var delta = e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0 ? 1 : -1;
+
+        if(delta < 0){
+            //mousewheel down handler
+            wheelDown = 1;
+            wheelUp = 0;
+            console.log("down");
+        }
+        else if(delta > 0){
+            //mousewheel up handler
+            wheelUp = 1;
+            wheelDown = 0;
+            console.log("up");
+        }
+        //prevent the default mousewheel scrolling
+        e.preventDefault();
+        return false;
+    });
+
+    //Move on click:
+    $('#arrow_prev a').click(function(e){
+
+        if ($(this).hasClass('arrowPrev') && pagePositon-1 >= 0) {
+            pagePositon--;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top
+              }, 300);
+            return false;
+        }
+    });
+    $('#arrow_next a').click(function(e){
+        if ($(this).hasClass('arrowNext') && pagePositon+1 <= pageMaxPosition) {
+            pagePositon++;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top
+            }, 300);
+            return false;
+        }
+    });
+    
+    //Update position func:
+    function upPos(){
+       var fromTop = $(this).scrollTop();
+       var $cur = null;
+        $scrollItems.each(function(index,ele){
+            if ($(ele).offset().top < fromTop + offsetTolorence) $cur = $(ele);
+        });
+       if ($cur != null && pagePositon != $cur.data('pos')) {
+           pagePositon = $cur.data('pos');
+       }                   
+    }
+
     /**********
     test function
     **********/
@@ -49,119 +112,6 @@ $(function(){
     /**********
     test function
     **********/
-
-    //Map the sections:
-    $scrollItems.each(function(index,ele) { $(ele).attr("debog",index).data("pos",index); });
-
-    // Bind to scroll
-    $(window).bind('scroll',upPos);
-    
-    // Mouse wheel down
-    $(document).on('mousewheel DOMMouseScroll', function (e) {
-        //prevent the default mousewheel scrolling
-        e.preventDefault();
-        //get the delta to determine the mousewheel scrol UP and DOWN
-        var delta = e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0 ? 1 : -1;
-
-        if(delta < 0){
-            //mousewheel down handler
-            wheelDown = 1;
-            wheelUp = 0;
-            console.log("down");
-            /*
-            if (pagePositon+1 <= pageMaxPosition) {
-                pagePositon++;
-                console.log("pagePositon "+pagePositon);
-                e.preventDefault();
-                $('html, body').stop().animate({ 
-                      scrollTop: $scrollItems.eq(pagePositon).offset().top
-                }, 300);
-            }
-            */
-            
-        }
-        else if(delta > 0){
-            //mousewheel up handler
-            wheelUp = 1;
-            wheelDown = 0;
-            console.log("up");
-            
-            /*
-            if (pagePositon-1 >= 0) {
-                pagePositon--;
-                console.log("pagePositon "+pagePositon);
-                e.preventDefault();
-                $('html, body').stop().animate({ 
-                    scrollTop: $scrollItems.eq(pagePositon).offset().top
-                }, 300);
-            }
-            */
-            
-        }
-        //prevent the default mousewheel scrolling
-        e.preventDefault();
-        return false;
-    });
-
-    /*
-    Using scroll true false
-    */
-    if(wheelUp){
-
-        if (pagePositon-1 >= 0) {
-            pagePositon--;
-            $('html, body').stop().animate({ 
-                  scrollTop: $scrollItems.eq(pagePositon).offset().top
-              }, 300);
-        }
-    }
-    if(wheelDown){
-        if (pagePositon+1 <= pageMaxPosition) {
-            pagePositon++;
-            $('html, body').stop().animate({ 
-                  scrollTop: $scrollItems.eq(pagePositon).offset().top
-            }, 300);
-        }
-    }
-
-
-    //Move on click:
-    $('#arrow_prev a').click(function(e){
-
-        if ($(this).hasClass('arrowPrev') && pagePositon-1 >= 0) {
-            pagePositon--;
-            $('html, body').stop().animate({ 
-                  scrollTop: $scrollItems.eq(pagePositon).offset().top
-              }, 300);
-            return false;
-        }
-    });
-    $('#arrow_next a').click(function(e){
-        if ($(this).hasClass('arrowNext') && pagePositon+1 <= pageMaxPosition) {
-            pagePositon++;
-            $('html, body').stop().animate({ 
-                  scrollTop: $scrollItems.eq(pagePositon).offset().top
-            }, 300);
-            return false;
-        }
-    });
-    /*
-    Using scroll true false
-    */
-    
-    //Update position func:
-    function upPos(){
-       var fromTop = $(this).scrollTop();
-       var $cur = null;
-        $scrollItems.each(function(index,ele){
-            if ($(ele).offset().top < fromTop + offsetTolorence) $cur = $(ele);
-        });
-       if ($cur != null && pagePositon != $cur.data('pos')) {
-           pagePositon = $cur.data('pos');
-       }                   
-    }
-
-
     
 });
 
